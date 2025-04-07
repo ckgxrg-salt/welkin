5a1ce9e5743c69ecc0f5e5b160fc43207c459e8a
ckgxrg <ckgxrg@ckgxrg.io>
2025-04-07 17:44:44 +0800

    Dispatcher: Allow forwarding

--- dispatcher/settings.nix
+++ dispatcher/settings.nix
@@ -8,6 +8,13 @@
     };
   };
 
+  # Allow forwarding
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv4.conf.all.forwarding" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+  };
+
   #========== Nix ==========#
   nix = {
     channel.enable = false;
